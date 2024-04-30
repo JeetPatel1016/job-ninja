15d449e9f40e95990440ed6bb43f780b9cc15a55
Jeet Patel <Jeetpatel1011@gmail.com>
2024-04-29 22:05:37 -0400

    Refactored Path ID in Job Listing Page

--- app/job/[id]/page.tsx
+++ app/job/[id]/page.tsx
@@ -2,8 +2,8 @@
 import { usePathname, useRouter } from "next/navigation";
 import { BiChevronLeft } from "react-icons/bi";
 
-export default function JobPage() {
-  const jobID = usePathname().split("/")[2];
+export default function JobPage({ params }: { params: { id: string } }) {
+  const jobID = params.id;
   const router = useRouter();
   return (
     <>
@@ -40,44 +40,43 @@ export default function JobPage() {
       </div>
       <div className="mt-8 px-4">
         <p className="whitespace-break-spaces">
-        ***MUST BE A CANADIAN CITIZEN OR A CANADIAN PERMANENT RESIDENT OR TO APPLY.***
-
-        Junior Developer Position
-
-        1. Technical Skills & Tools: Proficient in React, Express.js, Node.js, JavaScript, CSS, HTML5, and Git, which are all are . Ideally uses Microsoft VS Code to develop, and has a solid understanding of Photoshop for design purposes. Some knowledge of CI/CD practices and a DevOps approach is of benefit as well.
-
-        2. Modular Development & API Integration: Proficient in developing modular code with micro-functions, ensuring scalability and maintainability. Well versed in connecting to external APIs and seamlessly swapping in and out components of modular code. Understanding of API integration best practices and the ability to effectively utilize external resources in development projects.
-
-        3. Version Control & Testing: Well versed in GitHub and understands version control workflow. Experience in setting up and writing unit tests to prevent bugs.
-
-        4. Problem Solving & Critical Thinking: Solid understanding of algorithms and data structures, alongside the ability to use critical thinking to solve coding issues and debug effectively. The ability to pay meticulous attention to detail, and look at the big picture when solving problems or creating functions. Should have an understand of how to use large-language models for problem solving and research.
-
-        5. Teamwork & Communication: Can work effectively in a team, communicate ideas clearly and accepts feedback constructively. Demonstrates persistence, curiosity, and a mindset that embraces continuous learning and adaptation to new technologies and trends. Demonstrates good time management skills and the ability to work independently, taking initiative where needed.
-
-        6. UX/Design Principles: An understanding of user experience design and the ability to use tools like Photoshop for design work.
-
-        Job Type: Permanent
-
-        Pay: From $50,000.00 per year
-
-        Benefits:
-
-        Dental care
-        Extended health care
-        Paid time off
-        Flexible Language Requirement:
-
-        French not required
-        Schedule:
-
-        8 hour shift
-        Experience:
-
-        React, Express.js, Node.js, JavaScript, CSS, HTML5, Git.: 1 year (required)
-        Work Location: Remote
-        </p>  
+          ***MUST BE A CANADIAN CITIZEN OR A CANADIAN PERMANENT RESIDENT OR TO
+          APPLY.*** Junior Developer Position 1. Technical Skills & Tools:
+          Proficient in React, Express.js, Node.js, JavaScript, CSS, HTML5, and
+          Git, which are all are . Ideally uses Microsoft VS Code to develop,
+          and has a solid understanding of Photoshop for design purposes. Some
+          knowledge of CI/CD practices and a DevOps approach is of benefit as
+          well. 2. Modular Development & API Integration: Proficient in
+          developing modular code with micro-functions, ensuring scalability and
+          maintainability. Well versed in connecting to external APIs and
+          seamlessly swapping in and out components of modular code.
+          Understanding of API integration best practices and the ability to
+          effectively utilize external resources in development projects. 3.
+          Version Control & Testing: Well versed in GitHub and understands
+          version control workflow. Experience in setting up and writing unit
+          tests to prevent bugs. 4. Problem Solving & Critical Thinking: Solid
+          understanding of algorithms and data structures, alongside the ability
+          to use critical thinking to solve coding issues and debug effectively.
+          The ability to pay meticulous attention to detail, and look at the big
+          picture when solving problems or creating functions. Should have an
+          understand of how to use large-language models for problem solving and
+          research. 5. Teamwork & Communication: Can work effectively in a team,
+          communicate ideas clearly and accepts feedback constructively.
+          Demonstrates persistence, curiosity, and a mindset that embraces
+          continuous learning and adaptation to new technologies and trends.
+          Demonstrates good time management skills and the ability to work
+          independently, taking initiative where needed. 6. UX/Design
+          Principles: An understanding of user experience design and the ability
+          to use tools like Photoshop for design work. Job Type: Permanent Pay:
+          From $50,000.00 per year Benefits: Dental care Extended health care
+          Paid time off Flexible Language Requirement: French not required
+          Schedule: 8 hour shift Experience: React, Express.js, Node.js,
+          JavaScript, CSS, HTML5, Git.: 1 year (required) Work Location: Remote
+        </p>
       </div>
-      <button className="mx-4 mt-4 transition bg-blue-600 hover:bg-blue-600/80 rounded-md px-4 py-2">Open Posting Link</button>
+      <button className="mx-4 mt-4 transition bg-blue-600 hover:bg-blue-600/80 rounded-md px-4 py-2">
+        Open Posting Link
+      </button>
     </>
   );
 }
